9c4c69359f4773ae352a5de424e96bee37e7a7f6
Facundo <facundotisserasorribas@gmail.com>
2022-03-06 10:49:10 -0300

    lodader de pagina

---
+++ proyecto/js/loader.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",()=>{
+    
+    document.getElementById('onload').classList.toggle('centrado2');
+    
+     //const centrado = document.getElementById('onload');
+    // const body = document.querySelector('.hidden');
+    
+     //centrado.removeChild(centrado)
+    // body.classList.remove('hidden')
+
+})
